432aeca9e6d540584e49539ef39e651e3800401a
lh_intern <tclh123@gmail.com>
2014-02-14 00:19:29 +0800

    add database setup

--- quick_start.sh
+++ quick_start.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+echo "Setup database..."
+. ./setup_databases.sh
+
+echo "Init virtualenv..."
 virtualenv venv
 . venv/bin/activate
 pip install -U setuptools
 pip install cython
 pip install -r requirements.txt
+
+echo "Start serveing!!!"
+echo
+echo
 deactivate
 . venv/bin/activate
 gunicorn -b 0.0.0.0:8000 app:app
