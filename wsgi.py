13410168753409b6c8f496d7e2c6593e6b396bd7
XTao <xutao881001@gmail.com>
2014-02-17 22:13:18 +0800

    Add Werkzeug.

---
+++ wsgi.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# coding:utf8
+
+import logging
+from logging.handlers import RotatingFileHandler
+from werkzeug.serving import run_simple
+from werkzeug.wsgi import DispatcherMiddleware
+
+from app import app
+
+formatter = logging.Formatter(
+    "[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
+
+handler = RotatingFileHandler('code.log', maxBytes=10000, backupCount=1)
+handler.setLevel(logging.INFO)
+
+# werkzeug log
+#log = logging.getLogger('werkzeug')
+#log.setLevel(logging.DEBUG)
+#log.addHandler(handler)
+
+application = DispatcherMiddleware(app)
+
+if __name__ == "__main__":
+    run_simple('0.0.0.0', 8000, application,
+               use_reloader=True,
+               processes=2)
