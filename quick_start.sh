04b4944cb9407bc23a6a4ff7a10c8d9ba9a7e9ff
dongweiming <ciici1234@hotmail.com>
2014-02-17 07:49:35 +0000

    Fixed:
    1. When mysql_config not installed
    2. When virtualenv not installed
    3. Bugfix: Setup database Access denied

--- quick_start.sh
+++ quick_start.sh
@@ -1,18 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Setup database..."
 . ./setup_databases.sh
 
 echo "Init virtualenv..."
+which virtualenv > /dev/null 2>&1
+if [ $? != 0 ];then
+    echo "Install virtualenv..."
+    pip install virtualenv
+fi
 virtualenv venv
 . venv/bin/activate
 pip install -U setuptools
 pip install cython
+
+which mysql_config > /dev/null 2>&1
+if [ $? != 0 ];then
+    echo "mysql_config: command not found, must install it!"
+    exit 1
+fi
 pip install -r requirements.txt
 
 echo "Start serveing!!!"
 echo
 echo
-deactivate
-. venv/bin/activate
 gunicorn -w 2 -b 0.0.0.0:8000 app:app
