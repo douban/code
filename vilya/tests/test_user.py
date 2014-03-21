1b57df90f7310a8f0150747b4dbeebc7b00acb18
XTao <xutao881001@gmail.com>
2014-03-21 19:44:45 +0800

    WIP: add user test.

---
+++ vilya/tests/test_user.py
@@ -0,0 +1,22 @@
+from framework import *
+from unittest import TestCase
+from vilya.models.user import User
+
+
+class TestUser(TestCase):
+
+    def test_create(self):
+        password = "ilovecode"
+        name = "code"
+        desc = "description"
+        email = "code@douco.de"
+        user = User.create(name=name,
+                           password=password,
+                           description=desc,
+                           email=email)
+        self.assertEqual(user.name, name)
+        self.assertEqual(user.email, email)
+        self.assertEqual(user.description, desc)
+        self.assertTrue(user.validate_password(password))
+        self.assertEqual(len(user.projects), 0)
+        self.assertIsNone(user.org)
