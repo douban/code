e40832cd425a7640cb1115fd8fc9b69b1fe26712
XTao <xutao881001@gmail.com>
2014-03-21 18:49:25 +0800

    WIP: fix tests.

--- vilya/tests/test_project.py
+++ vilya/tests/test_project.py
@@ -5,7 +5,18 @@ from vilya.models.project import Project
 
 class TestProject(TestCase):
 
-    def test_add(self):
+    def test_create(self):
         proj_name = "test_project"
         proj = Project.create(name=proj_name, owner_id=1, creator_id=1)
-        assert proj.name == proj_name
+        self.assertEqual(proj.name, proj_name)
+        self.assertEqual(proj.owner_id, 1)
+        self.assertEqual(proj.creator_id, 1)
+        self.assertIsNotNone(proj.created_at)
+        self.assertIsNotNone(proj.updated_at)
+        self.assertIsNotNone(proj.repo)
+        self.assertIsNone(proj.upstream)
+        self.assertEqual(len(proj.boards), 5)
+        self.assertEqual(len(proj.card_boards), 3)
+        self.assertIsNotNone(proj.archive_board)
+        self.assertIsNotNone(proj.issue_board)
+        self.assertEqual(proj.remote_name, str(proj.id))
