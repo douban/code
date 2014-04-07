8d91ee87443feb71cdf34cb3cbc0cf41a1711931
qingfeng <qingfenghello@gmail.com>
2014-04-07 20:37:44 +0800

    reporter change to nyan

--- Gruntfile.js
+++ Gruntfile.js
@@ -153,7 +153,7 @@ module.exports = function (grunt) {
         mochaTest: {
           test: {
             options: {
-              reporter: 'spec'
+              reporter: 'nyan'
             },
             src: ['frontend/test/**/*.coffee']
           }
