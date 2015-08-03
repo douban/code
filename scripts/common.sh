ce11077e6a2e4af6a314471064a9f5cc3056b4c3
dongweiming <ciici1234@hotmail.com>
2015-08-03 23:31:36 +0800

    Add Dockerfile

--- scripts/common.sh
+++ scripts/common.sh
@@ -19,21 +19,23 @@ install_libmemcached() {
 check_virtualenv() {
     which virtualenv > /dev/null 2>&1
     if [ $? != 0 ];then
-	echo "Install virtualenv..."
-	sudo pip install virtualenv
+        echo "Install virtualenv..."
+        sudo pip install virtualenv
     fi
+    virtualenv venv
+    . venv/bin/activate
 }
 
 setup_database() {
     read -p "Please input Mysql User(default is root):" user
     if [ "${user}" = "" ];then
-	user="root"
+        user="root"
     fi
     read -p "Please input Mysql ${user}'s password(default is ''):" passwd
     echo "drop database if exists valentine" | mysql --user=${user} --password=${passwd}
     echo "create database valentine" | mysql --user=${user} --password=${passwd}
-    if [ $? -ne 0 ]; then
-	exit 1
+    if [b $? -ne 0 ]; then
+        exit 1
     fi
     (echo "use valentine"; cat vilya/databases/schema.sql) | mysql --user=${user} --password=${passwd}
 }
@@ -43,14 +45,9 @@ install_code() {
     cd code
     echo "Setup database..."
     setup_database
-    check_virtualenv
-    virtualenv venv
-    . venv/bin/activate
+    test "$CODE_ENV" != "unset" && check_virtualenv
     pip install cython  # should install first
     pip install -U setuptools  # python-libmemcached require updated setuptools
-    if [ -f /etc/arch-release ] ; then
-        pip install "distribute==0.6.29" # Fixed install MySQL-python on archlinux  for #14
-    fi
     pip install -r requirements.txt
 }
 
