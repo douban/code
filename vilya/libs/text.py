a9c87734639b15434a3c50e1d3c819a478ae3b06
XTao <xutao881001@gmail.com>
2016-02-07 00:14:25 +0800

    Remove douvatar

--- vilya/libs/text.py
+++ vilya/libs/text.py
@@ -76,8 +76,8 @@ def is_binary(fname):
 
 def gravatar_url(email, size=80):
     # 线上尺寸图已有size: (48, 64, 80)
-    default = "http://img3.douban.com/icon/user_normal.jpg"
-    url = "http://douvatar.dapps.douban.com/avatar/" + hashlib.md5(
+    default = "http://www.gravatar.com/avatar"
+    url = "http://www.gravatar.com/avatar/" + hashlib.md5(
         email.encode('utf8').lower()).hexdigest() + "?"
     url += urllib.urlencode({'d': default, 's': str(size), 'r': 'x'})
     return url
